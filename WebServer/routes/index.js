f111ed36429635e1ce55b46d16fb16765b04634c
Cavan2477 <liuyeying1103@163.com>
2016-07-13 23:32:24 +0800

    新增 1.fc.jade 2.sfc.jade 3.页面跳转
    修改
    1.导航栏正常显示

--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -13,25 +13,42 @@ var data_title = {
   title: '我爱emu',
 };
 
+// 路由规则
+module.exports = function(app) {
+  // index.jade
+  app.get('/', ShowIndex);
+  app.get('/index', ShowIndex);
+
+  // navigation.jade
+  app.get('/navigation', ShowNavigation);
+
+  // rom_mgr/fc
+  app.get('/rom_mgr/fc', ShowRomMgrFC);
+
+  // rom_mgr/sfc
+  app.get('/rom_mgr/sfc', ShowRomMgrSFC);
+};
+
 // Index.jade
-var ShowIndex = function(req, res){
+var ShowIndex = function(req, res) {
 	console.log('ShowIndex');
 	res.render('pages/index', data_title);
 };
 
 // Navigation.jade
-var ShowNavigation = function(req, res){
+var ShowNavigation = function(req, res) {
 	console.log('ShowNavigation');
 	res.render('pages/navigation', data_title);
 };
 
-// 路由规则
-module.exports = function(app){
-  // index.jade
-  app.get('/', ShowIndex);
-  app.get('/index', ShowIndex);
-
-  // header.jade
-  app.get('/navigation', ShowNavigation);
+// rom_mgr/fc.jade
+var ShowRomMgrFC = function(req, res) {
+  console.log('ShowRomMgrFC');
+  res.render('pages/rom_mgr/fc', data_title);
 };
 
+// rom_mgr/sfc.jade
+var ShowRomMgrSFC = function(req, res) {
+  console.log('ShowRomMgrSFC');
+  res.render('pages/rom_mgr/sfc', data_title);
+};
